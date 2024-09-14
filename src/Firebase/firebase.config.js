286ebf2396eefaff804c8a6e813cea30d3e983ab
unknown <abrarh1520444@gmail.com>
2024-09-15 01:31:06 +0600

    firebase api secured

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,14 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDl3UF6Btf00RJMpX7cJt7momqi7oZF92w",
-  authDomain: "jersey-geeks.firebaseapp.com",
-  projectId: "jersey-geeks",
-  storageBucket: "jersey-geeks.appspot.com",
-  messagingSenderId: "927592285373",
-  appId: "1:927592285373:web:0f98b054be4f622ffc23d4"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
